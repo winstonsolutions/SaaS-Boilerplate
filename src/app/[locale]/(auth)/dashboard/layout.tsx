f8e5afbd17ae9d02749c9be1f76d5617439a757a
WENTAO ZHAO <andyredjohn@gmail.com>
2025-07-10 12:53:37 -0500

    chore: change dashboard bg

--- src/app/[locale]/(auth)/dashboard/layout.tsx
+++ src/app/[locale]/(auth)/dashboard/layout.tsx
@@ -40,7 +40,7 @@ export default function DashboardLayout(props: { children: React.ReactNode }) {
         </div>
       </div>
 
-      <div className="min-h-[calc(100vh-72px)] bg-muted">
+      <div className="min-h-[calc(100vh-72px)] bg-gradient-to-b from-[#faf5ff] to-[#d8b4fe]">
         <div className="mx-auto max-w-screen-xl px-3 pb-16 pt-6">
           {props.children}
         </div>
